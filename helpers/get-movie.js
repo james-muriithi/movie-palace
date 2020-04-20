47aaf17daa35c4985cce27a6b5b82f9462e5bf22
james-muriithi <muriithijames556@gmail.com>
2020-04-20 21:14:14 +0300

    reduced countries to max 3

--- helpers/get-movie.js
+++ helpers/get-movie.js
@@ -11,7 +11,7 @@ let getMovie = async(id) => {
             res.genres.map(item => { gens.push(item.name) });
             res.production_countries.map(item => { country.push(item.name) });
             res.genres = gens
-            if (country.length > 3) { country = country.slice(1, 3) }
+            if (country.length > 3) { country = country.slice(1, 4) }
             res.production_countries = country
 
             if (res.keywords) {
