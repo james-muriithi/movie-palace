eabb58f8790bdca7ee42775580d78d8267b017d7
james-muriithi <muriithijames556@gmail.com>
2020-04-22 20:29:01 +0300

    added dotenv

--- helpers/urls.js
+++ helpers/urls.js
@@ -1,6 +1,7 @@
 const API_URL = 'https://api.themoviedb.org/3/';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
-const API_KEY = process.env.API_KEY || '';//moviedb api key
+require('dotenv').config();
+const API_KEY = process.env.API_KEY || ''; //moviedb api key
 
 const urls = {
     API_URL,
