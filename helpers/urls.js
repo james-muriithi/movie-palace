eb09e5ca3388f9f9dbeaea02e05377eb0c9cd3a0
james-muriithi <muriithijames556@gmail.com>
2020-04-22 22:53:30 +0300

    production bug fix

--- helpers/urls.js
+++ helpers/urls.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.themoviedb.org/3/';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const API_KEY = process.env.API_KEY || ''; //moviedb api key
 
 const urls = {
