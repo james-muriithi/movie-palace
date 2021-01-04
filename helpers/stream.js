edc61bf8d514cb10ddf2e0bf3a07ec1e5b2863a6
james-muriithi <muriithijames556@gmail.com>
2021-01-04 17:39:53 +0300

    added user agent header

--- helpers/stream.js
+++ helpers/stream.js
@@ -12,7 +12,8 @@ const iframeSrc = (id, season = '', episode = '') => {
 
     return axios.post(STREAM_URL, qs.stringify(params), {
         headers: {
-            'Content-Type': "application/x-www-form-urlencoded"
+            'Content-Type': "application/x-www-form-urlencoded",
+            'User-Agent': 'Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254'
         }
     }).then(data => {
         console.log(data);
